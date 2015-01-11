8a69ff8795606b52c8e7b2c55dbbbc2bb8f7b40c
Alexander Booth <alexanderbooth@alumni.ubc.ca>
2015-01-11 13:08:06 -0800

    organized imports

--- core/src/com/foxo/screens/LevelSelectionScreen.java
+++ core/src/com/foxo/screens/LevelSelectionScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
